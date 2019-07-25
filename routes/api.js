fcca6df34c8831c14fb5913da5406303a12c6e19
Anthony Zheng <ajz003@ucla.edu>
2019-07-24 23:43:23 -0700

    move iterator and message logic into .then, run prettier

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,8 @@ const axios = require("axios");
 var cheerio = require("cheerio");
 const client = require("twilio")(process.env.accountSid, process.env.authToken);
 
-const Discord = require('discord.js');
-const { prefix } = require('./../config.json');
+const Discord = require("discord.js");
+const { prefix } = require("./../config.json");
 const dcClient = new Discord.Client();
 
 let sports = [
@@ -13,6 +13,11 @@ let sports = [
     title: "Archery",
     url:
       "https://www.cosport.com/olympics/ticketsessiondetail.aspx?sportId=2&excludesoldout=False"
+  },
+  {
+    title: "Badminton",
+    url:
+      "https://www.cosport.com/olympics/ticketsessiondetail.aspx?sportId=6&excludesoldout=False"
   },
   {
     title: "Beach Volleyball",
@@ -48,6 +53,11 @@ let sports = [
     title: "Tennis",
     url:
       "https://www.cosport.com/olympics/ticketsessiondetail.aspx?sportId=35&excludesoldout=False"
+  },
+  {
+    title: "Closing Ceremony",
+    url:
+      "https://www.cosport.com/olympics/ticketsessiondetail.aspx?sportId=44&excludesoldout=False"
   }
 ];
 
@@ -62,7 +72,6 @@ router.get("/api/scrape-olympics", function(req, res) {
 
   let allResults = [];
   let allSportsSoldOut = true;
-
 
   function scrapeSport(iterator) {
     axios.get(sports[iterator].url).then(function(response) {
@@ -125,133 +134,14 @@ router.get("/api/scrape-olympics", function(req, res) {
       totalMessage += textMessage;
 
       allResults.push(results);
-    });
 
-    i++;
-
-    let min = 5000;
-    let max = 15000;
-    let range = max - min;
-    let randInterval = Math.random() * Math.floor(range) + min;
-
-    console.log(randInterval);
-    if (i < sports.length) {
-      setTimeout(function() {
-        scrapeSport(i);
-      }, randInterval);
-    } else {
-      setTimeout(() => {
-
-        if (!allSportsSoldOut) {
-          client.messages
-            .create({
-              body: totalMessage,
-              from: "***REMOVED***",
-              to: "***REMOVED***"
-            })
-            .then(function(response) {})
-            .done();
-
-        dcClient.once('ready', () => {
-          const channel = dcClient.channels.get('***REMOVED***');
-          try {
-          channel.send(totalMessage);            
-          }
-          catch(error) {
-            channel.send(console.error(error));
-          }
-        });
-        dcClient.login(process.env.DC_TOKEN);
-
-        } else {
-          console.log(totalMessage);
-        }
-
-
-        res.send(totalMessage);
-        
-      }, min + max);
-    }
-  }
-
-  scrapeSport(i);
-});
-
-router.get("/api/noon-test", function(req, res) {
-    let i = 0;
-  
-    let allResults = [];
-    let allSportsSoldOut = true;
-  
-    function scrapeSport(iterator) {
-      axios.get(sports[iterator].url).then(function(response) {
-        // Then, we load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(response.data);
-  
-        var results = {
-          results: []
-        };
-  
-        let sportTitle = $("span.discipline-title span.name").text();
-        let sportIsAllSoldOut = true;
-        let textMessage = "";
-  
-        $(".session-info").each(function(i, element) {
-          // Save an empty result object
-          var result = {};
-          result.sportTitle = sportTitle;
-          result.date = $(this)
-            .find("span.label")
-            .first()
-            .text();
-  
-          result.sessionCode = $(this)
-            .find(".session-code")
-            .text()
-            .replace("Session Code: ", "");
-  
-          result.info = [];
-  
-          let list = $(this).find("li.package");
-  
-          list.each(function(i, element) {
-            var ticketInfo = {};
-            ticketInfo.category = $(this)
-              .children("span.letter")
-              .text();
-            ticketInfo.price = $(this)
-              .children("span.text")
-              .text();
-  
-            if (ticketInfo.price !== "Sold Out" && sportIsAllSoldOut === true) {
-              sportIsAllSoldOut = false;
-              allSportsSoldOut = false;
-              textMessage += `${result.sportTitle} has tickets available at ${sports[iterator].url}!\n`;
-            }
-  
-            result.info.push(ticketInfo);
-          });
-  
-          console.log(result);
-  
-          results.results.push(result);
-        });
-  
-        if (sportIsAllSoldOut === true) {
-          textMessage = `No ${sports[iterator].title} tickets available.\n`;
-        }
-  
-        totalMessage += textMessage;
-  
-        allResults.push(results);
-      });
-  
       i++;
+
       let min = 5000;
       let max = 15000;
       let range = max - min;
       let randInterval = Math.random() * Math.floor(range) + min;
-  
+
       console.log(randInterval);
       if (i < sports.length) {
         setTimeout(function() {
@@ -259,6 +149,7 @@ router.get("/api/noon-test", function(req, res) {
         }, randInterval);
       } else {
         setTimeout(() => {
+          if (!allSportsSoldOut) {
             client.messages
               .create({
                 body: totalMessage,
@@ -267,22 +158,134 @@ router.get("/api/noon-test", function(req, res) {
               })
               .then(function(response) {})
               .done();
-              dcClient.once('ready', () => {
-                const channel = dcClient.channels.get('***REMOVED***');
-                try {
-                channel.send(totalMessage);            
-                }
-                catch(error) {
-                  channel.send(console.error(error));
-                }
-              });
-              dcClient.login(process.env.DC_TOKEN);
-            res.send(allResults);
+
+            dcClient.once("ready", () => {
+              const channel = dcClient.channels.get("***REMOVED***");
+              try {
+                channel.send(totalMessage);
+              } catch (error) {
+                channel.send(console.error(error));
+              }
+            });
+            dcClient.login(process.env.DC_TOKEN);
+          } else {
+            console.log(totalMessage);
+          }
+
+          res.send(totalMessage);
         }, min + max);
       }
-    }
-  
-    scrapeSport(i);
-  });
+    });
+  }
+
+  scrapeSport(i);
+});
+
+router.get("/api/noon-test", function(req, res) {
+  let i = 0;
+
+  let allResults = [];
+  let allSportsSoldOut = true;
+
+  function scrapeSport(iterator) {
+    axios.get(sports[iterator].url).then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      var results = {
+        results: []
+      };
+
+      let sportTitle = $("span.discipline-title span.name").text();
+      let sportIsAllSoldOut = true;
+      let textMessage = "";
+
+      $(".session-info").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+        result.sportTitle = sportTitle;
+        result.date = $(this)
+          .find("span.label")
+          .first()
+          .text();
+
+        result.sessionCode = $(this)
+          .find(".session-code")
+          .text()
+          .replace("Session Code: ", "");
+
+        result.info = [];
+
+        let list = $(this).find("li.package");
+
+        list.each(function(i, element) {
+          var ticketInfo = {};
+          ticketInfo.category = $(this)
+            .children("span.letter")
+            .text();
+          ticketInfo.price = $(this)
+            .children("span.text")
+            .text();
+
+          if (ticketInfo.price !== "Sold Out" && sportIsAllSoldOut === true) {
+            sportIsAllSoldOut = false;
+            allSportsSoldOut = false;
+            textMessage += `${result.sportTitle} has tickets available at ${sports[iterator].url}!\n`;
+          }
+
+          result.info.push(ticketInfo);
+        });
+
+        console.log(result);
+
+        results.results.push(result);
+      });
+
+      if (sportIsAllSoldOut === true) {
+        textMessage = `No ${sports[iterator].title} tickets available.\n`;
+      }
+
+      totalMessage += textMessage;
+
+      allResults.push(results);
+
+      i++;
+      let min = 5000;
+      let max = 15000;
+      let range = max - min;
+      let randInterval = Math.random() * Math.floor(range) + min;
+
+      console.log(randInterval);
+      if (i < sports.length) {
+        setTimeout(function() {
+          scrapeSport(i);
+        }, randInterval);
+      } else {
+        setTimeout(() => {
+          client.messages
+            .create({
+              body: totalMessage,
+              from: "***REMOVED***",
+              to: "***REMOVED***"
+            })
+            .then(function(response) {})
+            .done();
+          dcClient.once("ready", () => {
+            const channel = dcClient.channels.get("***REMOVED***");
+            try {
+              channel.send(totalMessage);
+            } catch (error) {
+              channel.send(console.error(error));
+            }
+          });
+          dcClient.login(process.env.DC_TOKEN);
+          res.send(allResults);
+        }, min + max);
+      }
+    });
+  }
+
+  scrapeSport(i);
+});
 
 module.exports = router;
